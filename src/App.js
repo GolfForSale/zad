17cf4177795b89372a90d3e1a384dc886514ac37
michal.gajoch <michal.gajoch@ge.com>
2021-11-10 19:24:54 +0100

    zz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [people, setPeople] = useState([{name: "Jan", surName: "Kowalski"}])
+    const [hover, setHover] = useState({})
+    const [isError, setError] = useState(false)
+    const [inputNameValue, setInputNameValue] = useState('')
+    const [inputSurNameValue, setInputSurNameValue] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if(!inputNameValue || !inputSurNameValue) {
+            setError(true)
+            return
+        }
+        setPeople([...people, {name: inputNameValue, surName: inputSurNameValue}])
+        setInputNameValue('')
+        setInputSurNameValue('')
+        setError(false)
+
+    }
+    const handleDeletePerson = (name, surName) => {
+        setPeople(people.filter(person=> person.name !== name && person.surName !== surName))
+    }
+
+    const onMouseEnterHandler = (name, surName) => {
+        setHover(people.find(person => person.name === name && person.surName === surName))
+    }
+    const onMouseLeaveHandler = () => {
+        setHover({})
+    }
+console.log('hover', hover)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="a" >A</div>
+        <div className="b" >B</div>
+        <div className="c" >C
+            <div className="e" >E</div>
+        </div>
+        <div className="f">
+            <div className="row">Pracownicy</div>
+            <ul className="peopleList">
+                {people.map(({name, surName, id}) => {
+                    const isHover = name === hover.name && surName === hover.surName
+                    return (
+                        <li className={`name row ${isHover && 'hovered'}`}>
+                            {`${name} ${surName}`}
+                            <button onMouseEnter={() => onMouseEnterHandler(name,surName)}
+                                    onMouseLeave={() => onMouseLeaveHandler(name,surName)}
+                                    onClick={() => handleDeletePerson(name,surName)}>X</button>
+                        </li>
+                    )
+                })}
+            </ul>
+            <form className="form"  onSubmit={handleSubmit}>
+                <label>
+                    Imię:
+                    <input className="input" type="text" value={inputNameValue} onChange={(ev) => setInputNameValue(ev.target.value)} />
+                </label>
+                <label>
+                    Nazwisko:
+                    <input className="input" type="text" value={inputSurNameValue} onChange={(ev) => setInputSurNameValue(ev.target.value)} />
+                </label>
+                {isError && <p>wypelnij poprawnie formularz</p>}
+                <input disabled={people.length > 5} type="submit" value="Wyślij" />
+            </form>
+        </div>
+        <div className="d" >D</div>
     </div>
   );
 }
